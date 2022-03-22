052346c4bfeffb40dd772537a65e1ef42c3d952f
RichieVan <richievan2@mail.ru>
2022-03-22 19:37:36 +0500

    Типы для компонентов Modal

---
+++ src/components/Modal/types.ts
@@ -0,0 +1,33 @@
+interface IModalBaseProps {
+  heading: string;
+  containerClickHandler(e: React.MouseEvent<HTMLDivElement, MouseEvent>): void;
+  closeModalHandler(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void;
+}
+
+interface IModalPostBaseProps {
+  containerClickHandler(e: React.MouseEvent<HTMLDivElement, MouseEvent>): void;
+  closeModalHandler(e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void;
+}
+
+interface IModalProps {
+  modalName: string;
+  heading: string;
+  temporal: boolean;
+}
+
+interface ILocationModalProps {
+  type: 'default' | 'post';
+  heading: string;
+  position: 'center' | 'start';
+  onClose: (() => void) | null;
+}
+
+type ModalCloseFunction = () => void;
+
+export {
+  IModalBaseProps,
+  IModalPostBaseProps,
+  IModalProps,
+  ILocationModalProps,
+  ModalCloseFunction,
+};
