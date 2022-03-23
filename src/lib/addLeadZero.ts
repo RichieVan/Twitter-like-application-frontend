95e12199ac1747c487a91dee3fb848f39f11f6a7
RichieVan <richievan2@mail.ru>
2022-03-23 19:59:44 +0500

    Типизация функции addLeadZero

--- src/lib/addLeadZero.ts
+++ src/lib/addLeadZero.ts
@@ -1,7 +1,7 @@
-export default (num) => {
+export default function addLeadZero(num: number): string {
   let convertedNum = String(num);
   if (convertedNum.length < 2) {
     convertedNum = `0${convertedNum}`;
   }
   return convertedNum;
-};
+}
