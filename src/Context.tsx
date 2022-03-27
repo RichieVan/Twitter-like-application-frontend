2656df25f788b327c6cb9757c4c2ae45981edd86
RichieVan <richievan2@mail.ru>
2022-03-27 19:59:44 +0500

    Удаление бесполезного хранилища UrlStore

--- src/Context.tsx
+++ src/Context.tsx
@@ -3,13 +3,11 @@ import { createContext } from 'react';
 import AppStore from './store/AppStore';
 import ModalStore from './store/ModalStore';
 import NotificationStore from './store/NotificationStore';
-import UrlStore from './store/UrlStore';
 import UserStore from './store/UserStore';
 import PostStore from './store/PostStore';
 
 const modalStore = new ModalStore();
 const notificationStore = new NotificationStore();
-const urlStore = new UrlStore(notificationStore);
 const userStore = new UserStore();
 const appStore = new AppStore(userStore);
 const postStore = new PostStore();
@@ -19,7 +17,6 @@ const storeList = {
   userStore,
   modalStore,
   notificationStore,
-  urlStore,
   postStore,
 };
 
