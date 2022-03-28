20c36a0e893f193e372c60e0545835ccc34eade6
RichieVan <richievan2@mail.ru>
2022-03-28 15:48:22 +0500

    Удаление лишних строк конфига

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
 const path = require('path');
 const webpack = require('webpack');
-const dotenv = require('dotenv');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-dotenv.config();
 require('dotenv').config({ path: './.env' });
 
 const modeStatus = (process.env?.BUILD_ENV === 'production') ? 'production' : 'development';
