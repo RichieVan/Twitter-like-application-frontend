5e0e939b3993ece959778c3914cd79c6ca1584a2
RichieVan <richievan2@mail.ru>
2022-04-02 12:16:11 +0500

    Обновление AuthRequired

--- src/components/AuthRequired.tsx
+++ src/components/AuthRequired.tsx
@@ -1,39 +1,25 @@
 import { observer } from 'mobx-react-lite';
 import React, { FC, useContext } from 'react';
-import { Navigate, To } from 'react-router-dom';
 
 import { Context } from '../Context';
 import { UserData } from '../types/types';
 
 interface AuthRedirectProps {
-  notAuthRedirect?: To,
-  notAuthElement?: JSX.Element;
-  render?: (userData: UserData) => JSX.Element;
+  renderOnNotAuth?: () => JSX.Element;
+  renderOnAuth?: (userData: UserData) => JSX.Element;
 }
 
 const AuthRequired: FC<AuthRedirectProps> = ({
-  notAuthRedirect,
-  notAuthElement,
-  children,
-  render,
+  renderOnNotAuth,
+  renderOnAuth,
 }): JSX.Element | null => {
   const { userStore } = useContext(Context);
   let result: JSX.Element | null = null;
 
-  if (userStore.user) {
-    if (render) result = render(userStore.user);
-    else if (React.isValidElement(children)) result = children;
-  } else {
-    if (notAuthRedirect) {
-      result = (
-        <Navigate
-          to={notAuthRedirect}
-          state={{ authRedirected: true }}
-          replace
-        />
-      );
-    }
-    if (notAuthElement) result = notAuthElement;
+  if (userStore.user && renderOnAuth) {
+    result = renderOnAuth(userStore.user);
+  } else if (renderOnNotAuth) {
+    result = renderOnNotAuth();
   }
 
   return result;
