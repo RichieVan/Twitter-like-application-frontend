0b4ef55265d3f8ce067d1cc85e8ebf5c0ac5f803
RichieVan <richievan2@mail.ru>
2022-04-25 19:31:54 +0500

    Удаление комментариев

--- webpack.config.js
+++ webpack.config.js
@@ -58,7 +58,7 @@ module.exports = {
           {
             loader: 'react-svg-loader',
             options: {
-              jsx: true, // true outputs JSX tags
+              jsx: true,
             },
           },
         ],
