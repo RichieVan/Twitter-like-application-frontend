8b1109606d2a3f5d2568330ab38fd22757e6496d
RichieVan <richievan2@mail.ru>
2022-06-16 14:52:38 +0500

    Подключение компонента reduxtest в индексном файле приложения

--- src/index.tsx
+++ src/index.tsx
@@ -4,15 +4,23 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './styles/style.scss';
 
+import { Provider } from 'react-redux';
 import App from './App';
 import { Context, storeList } from './Context';
+import { store as ReduxStore } from './store';
+import Reduxtest from './components/reduxtest';
 
 const application = (
-  <Context.Provider value={storeList}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Context.Provider>
+  <>
+    <Provider store={ReduxStore}>
+      <Reduxtest />
+    </Provider>
+    <Context.Provider value={storeList}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Context.Provider>
+  </>
 );
 
 ReactDOM.render(
