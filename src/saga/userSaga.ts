392b2b27c63c775287b44f71c427373f35beda87
RichieVan <richievan2@mail.ru>
2022-06-24 14:42:27 +0500

    Добавление недостающих саг, вынос обновления пользователя и токена в отдельный генератор

--- src/saga/userSaga.ts
+++ src/saga/userSaga.ts
@@ -5,27 +5,70 @@ import {
   all,
 } from 'redux-saga/effects';
 import { PayloadAction } from '@reduxjs/toolkit';
-import { AxiosResponse } from 'axios';
-import { UserDataWithTokens, UserLoginData } from '../types/types';
+import axios, { AxiosResponse } from 'axios';
+import {
+  ApiErrorData,
+  UserDataWithTokens,
+  UserLoginData,
+  UserRegistrationData,
+  UserUpdateData,
+} from '../types/types';
 import { clearUser, setUser } from '../store/reducers/userReducer/userReducer';
 import UserService from '../services/UserService';
 import ErrorHelper from '../helpers/ErrorHelper';
-import { ASYNC_USER_LOGIN, ASYNC_USER_LOGOUT } from '../store/reducers/userReducer/types';
+import {
+  USER_LOGIN,
+  USER_LOGOUT,
+  USER_REGISTRATION,
+  USER_UPDATE,
+} from '../store/reducers/userReducer/types';
 import TokenService from '../services/TokenService';
+import { setLoginErrors, setRegistrationErrors } from '../store/reducers/formReducer/formReducer';
+import isAxiosError from '../guards/isAxiosError';
+import { API_URL } from '../http';
 
-function* userLoginWorker({ payload: loginData }: PayloadAction<UserLoginData>) {
+function* setTokensAndUserData({ user, accessToken }: UserDataWithTokens) {
+  TokenService.setAccessToken(accessToken);
+  yield put(setUser({ user }));
+}
+
+function* login({ payload }: PayloadAction<UserLoginData>) {
   try {
-    const {
-      data: { user, accessToken },
-    }: AxiosResponse<UserDataWithTokens> = yield call(UserService.login, loginData);
-    TokenService.setAccessToken(accessToken);
-    yield put(setUser({ user }));
+    const { data }: Awaited<ReturnType<typeof UserService.login>> = yield call(
+      UserService.login,
+      payload,
+    );
+
+    yield setTokensAndUserData(data);
   } catch (e) {
-    ErrorHelper.handleApiError(e);
+    if (isAxiosError<ApiErrorData>(e) && e.response) {
+      const { message, errors } = e.response.data;
+      yield put(setLoginErrors([message, ...errors]));
+    } else {
+      ErrorHelper.handleUnexpectedError();
+    }
   }
 }
 
-function* userLogoutWorker() {
+function* registration({ payload }: PayloadAction<UserRegistrationData>) {
+  try {
+    const { data }: Awaited<ReturnType<typeof UserService.registration>> = yield call(
+      UserService.registration,
+      payload,
+    );
+
+    yield setTokensAndUserData(data);
+  } catch (e) {
+    if (isAxiosError<ApiErrorData>(e) && e.response) {
+      const { message, errors } = e.response.data;
+      yield put(setRegistrationErrors([message, ...errors]));
+    } else {
+      ErrorHelper.handleUnexpectedError();
+    }
+  }
+}
+
+function* logout() {
   try {
     yield call(UserService.logout);
     TokenService.clearAccessToken();
@@ -35,9 +78,33 @@ function* userLogoutWorker() {
   }
 }
 
+function* updateData({ payload }: PayloadAction<UserUpdateData>) {
+  const { data }: Awaited<ReturnType<typeof UserService.updateUser>> = yield call(
+    UserService.updateUser,
+    payload,
+  );
+  yield setTokensAndUserData(data);
+}
+
+export function* checkAuthorization() {
+  try {
+    const { data }: AxiosResponse<UserDataWithTokens> = yield call(
+      axios.get,
+      `${API_URL}/refresh`,
+      { withCredentials: true },
+    );
+
+    yield setTokensAndUserData(data);
+  } catch (e) {
+    TokenService.clearAccessToken();
+  }
+}
+
 function* loginFlow() {
-  yield takeLeading(ASYNC_USER_LOGIN, userLoginWorker);
-  yield takeLeading(ASYNC_USER_LOGOUT, userLogoutWorker);
+  yield takeLeading(USER_LOGIN, login);
+  yield takeLeading(USER_LOGOUT, logout);
+  yield takeLeading(USER_REGISTRATION, registration);
+  yield takeLeading(USER_UPDATE, updateData);
 }
 
 export default function* userWatcher() {
