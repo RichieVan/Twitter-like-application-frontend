f7e67f07204bd47ce6bae78748500f3ff027f7c3
RichieVan <richievan2@mail.ru>
2022-06-26 19:43:05 +0500

    Добавление селекторов для feedStore

--- src/store/reducers/feedReducer/selectors.ts
+++ src/store/reducers/feedReducer/selectors.ts
@@ -1,10 +1,25 @@
 import { Selector } from '@reduxjs/toolkit';
 import { PostData } from '../../../types/types';
 import { FeedState, FeedType } from './types';
+import type { RootState } from '../../index';
 
-export const selectFeedFirstLoaded: Selector<FeedState, PostData | null> = ({
-  firstLoaded,
-}) => firstLoaded;
+export const selectFeed: Selector<RootState, FeedState> = ({ feed }) => feed;
 
-export const selectFeedType: Selector<FeedState, FeedType> = ({ type }) => type;
-export const selectFeedPosts: Selector<FeedState, PostData[]> = ({ posts }) => posts;
+export const selectFeedLoading: Selector<RootState, boolean> = (
+  state,
+) => selectFeed(state).loading;
+export const selectFeedSyncing: Selector<RootState, boolean> = (
+  state,
+) => selectFeed(state).syncing;
+export const selectFeedFirstLoaded: Selector<RootState, PostData | null> = (
+  state,
+) => selectFeed(state).firstLoaded;
+export const selectFeedType: Selector<RootState, FeedType> = (
+  state,
+) => selectFeed(state).type;
+export const selectFeedPosts: Selector<RootState, PostData[]> = (
+  state,
+) => selectFeed(state).posts;
+export const selectFeedCanLoadMore: Selector<RootState, boolean> = (
+  state,
+) => selectFeed(state).canLoadMore;
