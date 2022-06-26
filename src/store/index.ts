71f4f47a091a228abe7f64b2443a909f125d9c08
RichieVan <richievan2@mail.ru>
2022-06-26 19:42:32 +0500

    Добавление form, app, post и feed редюсеров в redux стор

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,12 +2,20 @@ import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
 import rootWatcher from '../saga';
 import userReducer from './reducers/userReducer/userReducer';
+import formReducer from './reducers/formReducer/formReducer';
+import appReducer from './reducers/appReducer/appReducer';
+import postReducer from './reducers/postReducer/postReducer';
+import feedReducer from './reducers/feedReducer/feedReducer';
 
 const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer: {
+    app: appReducer,
     user: userReducer,
+    form: formReducer,
+    post: postReducer,
+    feed: feedReducer,
   },
   middleware: [
     sagaMiddleware,
