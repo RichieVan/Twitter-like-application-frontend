49e0875235b1c0b0fd12302c2eb88044ea374c37
RichieVan <richievan2@mail.ru>
2022-06-26 19:42:00 +0500

    Добавление postWatcher в список саг rootWatcher

--- src/saga/index.ts
+++ src/saga/index.ts
@@ -1,9 +1,14 @@
 import { all } from 'redux-saga/effects';
 import userWatcher from './userSaga';
-import postWatcher from './feedSaga';
+import feedWatcher from './feedSaga';
+import postWatcher from './postSaga';
 
 function* rootWatcher() {
-  yield all([userWatcher(), postWatcher()]);
+  yield all([
+    userWatcher(),
+    postWatcher(),
+    feedWatcher(),
+  ]);
 }
 
 export default rootWatcher;
